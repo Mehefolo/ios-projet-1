f4cde16de77c9f7e58a18601e903600fce3db1da
ESATIC FORMATION <esaticformation@192.168.1.4>
2021-03-30 14:15:32 +0000

    TP1

--- TP1/ViewController.swift
+++ TP1/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var field2: UITextField!
+    
+  
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onClick(_ sender: UIButton) {
+        
+        result.text="resultat :"
+        
+        let nb1 = Double(field1.text!) ?? 0
+        let nb2 = Double(field2.text!) ?? 0
+        
+        let sum = nb1 + nb2
+        
+        result.text = "\(String(describing: result.text!))"+"\(sum)"
+        
+    }
+    
+    
+    @IBAction func cleanClick(_ sender: UIButton) {
+        
+        field1.text = ""
+        field2.text = ""
+        result.text = ""
+    }
+    
 }
 
